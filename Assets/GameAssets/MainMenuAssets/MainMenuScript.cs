49b1c782eecfa9fa83b8c5f61e91db0d67b8783b
Jeremy <adgrygry12348@gmail.com>
2021-05-21 12:07:55 +0700

    i did my best
    Add MainMenuScene and GameScene
    
    -ToDo:
    (+) Actual Game

---
+++ Assets/GameAssets/MainMenuAssets/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+#if DEBUG
+        Debug.Log("Application Exitted");
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
