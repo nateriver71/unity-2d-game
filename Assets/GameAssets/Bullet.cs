2323625736b50d7711b924cb9b3bc4a36599022c
Jeremy <adgrygry12348@gmail.com>
2021-06-05 22:34:49 +0700

    Fix simple aim and shoot function
    (+) Fix shooting trajectory and destroying object on collision
    
    ToDo:
    - Overhaul MainMenu
    - Add Enemy (behaviour and movement)

--- Assets/GameAssets/Bullet.cs
+++ Assets/GameAssets/Bullet.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
-    private float speed = 5f;
-    private Rigidbody2D bulletRb;
-    public Vector2 moveDirection;
+    public Vector2 velocity = new Vector2(0.0f, 0.0f);
+    public GameObject originParent;
     // Start is called before the first frame update
     void Start()
     {
-        bulletRb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
+        Vector2 currentPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        Vector2 newPosition = currentPosition + velocity * Time.deltaTime;
+
+        Debug.DrawLine(currentPosition, newPosition, Color.red);
+
+        RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition, newPosition);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject != originParent)
+            {
+                Destroy(gameObject);
+
+                // ToDo: Add Player Health Check
+            }
+        }
+
+        this.transform.position = newPosition;
     }
 }
--- Assets/GameAssets/GunManager.cs
+++ Assets/GameAssets/GunManager.cs
@@ -6,38 +6,23 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class GunManager : MonoBehaviour
 {
-    
-    public GameObject shootTargetPrefab;
-    private GameObject shootTarget;
-    private Transform playerTransform;
-    private Rigidbody2D playerRb;
-    private Vector2 move_direction;
-    private float delta = 10f;
-    private float delayShoot = 0.1f;
-
-    //prefab object to shoot
+    [Header("Shooting projection")]
+    public GameObject crosshair;
     public GameObject projectile;
+
+    private float ARROW_BASE_SPEED = 75.0f;
+    private float SHOOT_TIME_DELAY = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = GetComponent<Transform>();
-        playerRb = GetComponent<Rigidbody2D>();
-        shootTarget = Instantiate<GameObject>(shootTargetPrefab);
-        shootTarget.transform.position = playerTransform.position;
-
         StartCoroutine(AsyncGetKey());
     }
 
     // Update is called once per frame
     void Update()
     {
-        move_direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (move_direction != Vector2.zero)
-        {
-            Debug.DrawRay(playerTransform.position, move_direction * 5.0f, Color.red);
-            shootTarget.transform.position = playerTransform.position + new Vector3(move_direction.x * delta, move_direction.y * delta, 0);
-        }
+        MoveCrosshair();
     }
 
     void FixedUpdate()
@@ -45,28 +30,47 @@ public class GunManager : MonoBehaviour
 
     }
 
+    void MoveCrosshair()
+    {
+        Vector3 aim = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        if (aim.magnitude > 0.0f)
+        {
+            aim.Normalize();
+            aim *= 0.65f;
+            crosshair.transform.localPosition = aim;
+            crosshair.SetActive(true);
+        }
+        else
+        {
+            crosshair.SetActive(false);
+        }
+    }
+
     void Shoot()
     {
-        Vector2 shootDirection = move_direction.normalized;
-        if (shootDirection != Vector2.zero)
-        {
-            GameObject arrows = Instantiate(projectile, playerTransform.position, Quaternion.identity);
-            //arrows.GetComponent<Rigidbody2D>().velocity = shootDirection * 5.0f/*speed*/;
-            arrows.GetComponent<Bullet>().moveDirection = shootDirection * 5.0f;
-            arrows.transform.Rotate(0, 0, Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg);
-            Destroy(arrows, 2f);
-        }
+        Vector2 shootDirection = crosshair.transform.localPosition;
+        shootDirection.Normalize();
+
+        GameObject bullet_ = Instantiate(projectile, this.transform.position, Quaternion.identity);
+        Bullet bullet = bullet_.GetComponent<Bullet>();
+        bullet.velocity = shootDirection * ARROW_BASE_SPEED;
+        bullet.originParent = gameObject;
+        bullet_.transform.Rotate(0, 0, Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg);
+
+        Destroy(bullet_, 10f); // destroy bullet after 10 second if stuck or not doing anything
     }
 
     IEnumerator AsyncGetKey()
     {
         while (true)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space)) // Hard coded maybe use more input system method
             {
+                Debug.Log("Shoot");
                 Shoot();
             }
-            yield return new WaitForSeconds(delayShoot);
+            yield return new WaitForSeconds(SHOOT_TIME_DELAY);
         }
     }
 }
