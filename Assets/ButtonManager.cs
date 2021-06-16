3424a1e999fb428cbb7c756f73326a141a20bcd7
Jeremy <adgrygry12348@gmail.com>
2021-06-16 15:32:57 +0700

    Initial Commit

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
+}
