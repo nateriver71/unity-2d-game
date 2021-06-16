223342de9a0f9b418ec26bdb3533defbf554c9ee
Jeremy <adgrygry12348@gmail.com>
2021-06-16 17:00:31 +0700

    Add Loading Scene

---
+++ Assets/LoadingManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadingManagerScript : MonoBehaviour
+{
+    public TextMeshProUGUI loading_text;
+
+    private string loading_dots = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startAnim());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator startAnim()
+    {
+        while (true)
+        {
+            if (loading_dots.Length <= 3)
+            {
+                loading_dots += ".";
+            }
+            else
+            {
+                loading_dots = ".";
+            }
+
+            loading_text.text = "Loading"+loading_dots;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
